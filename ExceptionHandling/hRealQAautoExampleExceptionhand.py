3248ba9168167f7705ba54c1c990bf3b71792921
Prashanta <prashantkumarbehera1@gmail.com>
2026-03-21 12:34:30 +0530

    new exception handling programs

---
+++ ExceptionHandling/hRealQAautoExampleExceptionhand.py
@@ -0,0 +1,8 @@
+def get_test_data(index):
+    data= ["login", "signup"]
+    try:
+        return data[index]
+    except IndexError:
+        return "Invalid test case index"
+        
+        print(get_test_data(5))
